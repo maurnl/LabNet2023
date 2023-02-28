8b2662b083984e85950cc5739ff21c383cb0c97b
maurnl <mauronluciano@hotmail.com>
2023-02-27 22:15:39 -0300

    ReadKey method call added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
+            Console.ReadKey();
         }
     }
 }
