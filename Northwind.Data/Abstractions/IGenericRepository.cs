c706f2ccd3f994cbcd1852005ec463199aab452f
maurnl <mauronluciano@hotmail.com>
2023-03-13 16:07:46 -0300

    agrego repositorys en capa de datos

---
+++ Northwind.Data/Abstractions/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Data.Abstractions
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ Northwind.Data/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Northwind.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly NorthwindContext _nwContext;
+
+        public GenericRepository()
+        {
+            _nwContext = new NorthwindContext();
+        }
+
+        public void Add(T entity)
+        {
+            _nwContext.Set<T>().Add(entity);
+            _nwContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _nwContext.Entry(entity).State = EntityState.Modified;
+            _nwContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _nwContext.Set<T>().Remove(entity);
+            _nwContext.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _nwContext.Set<T>().AsQueryable() ?? Enumerable.Empty<T>().AsQueryable();
+        }
+    }
+}
